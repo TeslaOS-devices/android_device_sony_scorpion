80a419d131f8ccf1cc1b8d1b42bb23e63246498b
NoelMacwan <yulespencer@gmail.com>
2015-03-28 23:05:27 +0530

    Tesla Bringup
    Signed-off-by: NoelMacwan <yulespencer@gmail.com>

--- tesla.mk
+++ tesla.mk
@@ -1,8 +1,8 @@
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit Tesla common Phone stuff.
+$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/tesla/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/sony/scorpion/full_scorpion.mk)
 
@@ -10,5 +10,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGP621
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/SGP621/SGP621:5.0.2/23.1.A.0.690/4135415812:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="SGP621-user 5.0.2 23.1.A.0.690 4135415812 release-keys"
 
-PRODUCT_NAME := cm_scorpion
+PRODUCT_NAME := tesla_scorpion
 PRODUCT_DEVICE := scorpion
